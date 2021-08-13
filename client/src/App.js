cff81b7fa62c3c5b758f016fb0d6f44079767124
Ben_Akiva <dartaryan@gmail.com>
2021-08-13 15:04:33 +0300

    styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,40 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import fadeories from "./images/fadeories.gif";
+
+import Posts from "./components/Posts/Posts";
+import Post from "./components/Posts/Post/Post";
+import Form from "./components/Form/Form";
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Fadeories
+        </Typography>
+        <img src={fadeories} alt="fadeories" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="strech"
+            spaceing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
+//  light blue: #83adec
 
 export default App;
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -12,11 +12,15 @@ export const getPosts = async (req, res) => {
   }
 };
 
-export const createPost = (req, res) => {
-const body = req.body;
-    try {
-    
-} catch (error) {
-    
-}
+export const createPost = async (req, res) => {
+  const post = req.body;
+
+  const newPost = new PostMessage(post);
+  try {
+    await newPost.save();
+
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
 };
