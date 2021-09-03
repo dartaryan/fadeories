304976ace58f8078245cbd06bce5111776ae26da
Ben_Akiva <dartaryan@gmail.com>
2021-09-03 14:57:46 +0300

    Pagination

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import { Navbar } from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Auth from "./components/Auth/Auth";
 
+
+
 const App = () => (
   <BrowserRouter>
     <Container maxWidth="lg">
