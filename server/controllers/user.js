56be8f966c80fc7180eb4ca71ece6f55c05aa2cb
Ben_Akiva <dartaryan@gmail.com>
2021-09-03 13:27:17 +0300

    fixing db

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -6,10 +6,14 @@ export const signin = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const existingUser = await User.findOne({ email });
+    const existingUser = await User.findOne({
+      email,
+    });
 
     if (!existingUser)
-      return res.status(404).json({ message: "User doesn't exist." });
+      return res.status(404).json({
+        message: "User doesn't exist.",
+      });
 
     const isPasswordCorrect = await bcrypt.compare(
       password,
@@ -17,16 +21,28 @@ export const signin = async (req, res) => {
     );
 
     if (!isPasswordCorrect)
-      return res.status(400).json({ message: "Invalid credentials" });
+      return res.status(400).json({
+        message: "Invalid credentials",
+      });
 
     const token = jwt.sign(
-      { email: existingUser.email, id: existingUser._id },
+      {
+        email: existingUser.email,
+        id: existingUser._id,
+      },
       "test",
-      { expiresIn: "1h" }
+      {
+        expiresIn: "1h",
+      }
     );
-    res.status(200).json({ result: existingUser, token });
+    res.status(200).json({
+      result: existingUser,
+      token,
+    });
   } catch (error) {
-    res.status(500).json({ message: "Something went wrong." });
+    res.status(500).json({
+      message: "Something went wrong.",
+    });
   }
 };
 
@@ -34,13 +50,19 @@ export const signup = async (req, res) => {
   const { email, password, confirmPassword, firstName, lastName } = req.body;
 
   try {
-    const existingUser = await User.findOne({ email });
+    const existingUser = await User.findOne({
+      email,
+    });
 
     if (existingUser)
-      return res.status(400).json({ message: "User already exists." });
+      return res.status(400).json({
+        message: "User already exists.",
+      });
 
     if (password !== confirmPassword)
-      return res.status(400).json({ message: "Passwords don't match." });
+      return res.status(400).json({
+        message: "Passwords don't match.",
+      });
 
     const hashedPassword = await bcrypt.hash(password, 12);
     const result = await User.create({
@@ -49,11 +71,23 @@ export const signup = async (req, res) => {
       name: `${firstName} ${lastName}`,
     });
 
-    const token = jwt.sign({ email: result.email, id: result._id }, "test", {
-      expiresIn: "1h",
+    const token = jwt.sign(
+      {
+        email: result.email,
+        id: result._id,
+      },
+      "test",
+      {
+        expiresIn: "1h",
+      }
+    );
+    res.status(200).json({
+      result: result,
+      token,
     });
-    res.status(200).json({ result: result, token });
   } catch (error) {
-    res.status(500).json({ message: "Something went wrong." });
+    res.status(500).json({
+      message: "Something went wrong.",
+    });
   }
 };
--- server/index.js
+++ server/index.js
@@ -2,35 +2,36 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js";
 import userRoutes from "./routes/users.js";
 
 const app = express();
-dotenv.config();
+// dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
 app.use("/posts", postRoutes);
-app.use('/user',userRoutes)
+app.use("/user", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Welcome back to fadeories API");
 });
 
-const CONNECTION_URL = "mongodb+srv://dartaryan:305065575@cluster0.ewktg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const CONNECTION_URL =
+  "mongodb+srv://dartaryan:305065575@cluster0.ewktg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.CONNECTION_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
-    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
   )
-  .catch((error) => error.message);
+  .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set("useFindAndModify", false);
