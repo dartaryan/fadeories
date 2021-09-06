5599a5dbf595b35b8f5f798d45ac1f07109c005a
Ben_Akiva <dartaryan@gmail.com>
2021-09-06 03:38:04 +0300

    deploying

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// const API = axios.create({ baseURL: "https://fadeories.herokuapp.com" });
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://fadeories.herokuapp.com/" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
